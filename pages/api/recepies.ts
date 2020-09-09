5ca3c8a5593d42c25b03c37936c9e514e7efa639
Milan Kalinic <kalinicm@gmail.com>
2020-09-09 12:06:44 +0200

    Simplify API and style html, add svg example

--- pages/api/recepies.ts
+++ pages/api/recepies.ts
@@ -1,6 +1,9 @@
 import Axios from 'axios';
 
-export async function apiRecepies() {
+export interface apiRecepiesI {
+  title: string;
+}
+export async function apiRecepies(): Promise<apiRecepiesI> {
   const url = `http://localhost:3000/api/recepies`;
   const [err, data] = await Axios.get(url)
     .then((r) => [null, r?.data])
@@ -10,13 +13,58 @@ export async function apiRecepies() {
   return output;
 }
 
-export default async function recepies(req, res) {
-  const output = {
-    title: 'Search recepies',
-  };
-  res.status(200).json(output);
+export interface apiRecepiesSearchI {
+  title: string;
+  version: number;
+  href: string;
+  results: {
+    title: string;
+    href: string;
+    ingredients: string;
+    thumbnail: string;
+  }[];
+}
+export async function apiRecepiesSearch({ value }: { value: string }): Promise<apiRecepiesSearchI> {
+  const valuePlus = value?.split(' ').join('+');
+  const url = `http://localhost:3000/api/recepies?search=${valuePlus}`;
+  const [err, data] = await Axios.get(url)
+    .then((r) => [null, r?.data])
+    .catch((e) => [e]);
+  const output = err ? {} : data;
+
+  return output;
 }
 
-export interface apiRecepiesI {
-  title: string;
+export default async function recepies(req, res) {
+  const { query, method } = req || {};
+  const methodLowercase = method?.toLowerCase(); // POST,Post,post > post
+
+  let output;
+  if (methodLowercase === 'get') {
+    // GET EXISTING RECORDS
+    if (query?.search) {
+      // multiple records
+      const q = query?.search?.split(' ').join('+') || '';
+      const [err, data] = await Axios.get(`http://www.recipepuppy.com/api/?q=${q}`)
+        .then((r) => [null, r?.data])
+        .catch((e) => [e]);
+      output = err ? {} : data;
+    } else if (query.id) {
+      // single record
+    } else {
+      // initial data
+      output = {
+        title: 'Search recepies',
+      };
+    }
+  } else if (methodLowercase === 'post') {
+    // ADD NEW RECORD
+  } else if (methodLowercase === 'put') {
+    // REPLACE EXISTING RECORD
+  } else if (methodLowercase === 'patch') {
+    // UPDATE EXISTING RECORD
+  } else if (methodLowercase === 'delete') {
+    // DELETE EXISTING RECORD
+  }
+  res.status(200).json(output);
 }
--- pages/recepies.tsx
+++ pages/recepies.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { apiRecepies, apiRecepiesI } from '@/pages/api/recepies';
-import { apiRecepiesSearch, apiRecepiesSearchI } from '@/pages/api/recepies-search';
+import { apiRecepies, apiRecepiesI, apiRecepiesSearch, apiRecepiesSearchI } from '@/pages/api/recepies';
 
 export default function Recepti(props: apiRecepiesI) {
   const { title } = props;
@@ -12,7 +11,7 @@ export default function Recepti(props: apiRecepiesI) {
         <h1 className="text-3xl font-bold text-pink-500">{title}</h1>
         <div>
           <input
-            className="border border-gray-400 p-3 rounded-lg text-lg"
+            className="p-3 text-lg border border-gray-400 rounded-lg"
             placeholder="search..."
             onChange={async (e) => {
               setReceipes(await apiRecepiesSearch({ value: e.target.value }));
@@ -22,21 +21,30 @@ export default function Recepti(props: apiRecepiesI) {
       </div>
       {receipes && (
         <div>
-          <h2 className="text-2xl font-bold mt-2">Recepti</h2>
+          <h2 className="mt-2 text-2xl font-bold">Recepti</h2>
           <div className="flex flex-col text-sm">
             {receipes?.results?.map((r) => {
               return (
-                <div className="flex">
-                  <div className="w-20 h-20 bg-gray-200 mb-2">
-                    {r.thumbnail && <img className="w-20 h-20" alt={r.title} src={r.thumbnail} />}
+                <div className="flex mt-2 divide-y divide-gray-300" key={r.href}>
+                  <div className="w-1/6 mb-2 bg-gray-200">
+                    {r.thumbnail && <img className="object-cover w-full h-full" alt={r.title} src={r.thumbnail} />}
                   </div>
-                  <div className="flex flex-col p-2 ">
-                    <div className="font-bold text-xl tracking-tight">
+                  <div className="flex flex-col w-5/6 p-2 pl-4">
+                    <div className="text-2xl font-bold leading-none tracking-tight">
                       <a href={r.href} target="_blank" rel="noreferrer">
                         {r.title}
                       </a>
                     </div>
-                    <div>{r?.href?.slice(0, 30)}...</div>
+
+                    <div className="mt-1">{r?.ingredients}</div>
+                    <div className="flex justify-start text-gray-600">
+                      <div className="text-right">
+                        <svg className="w-4 h-4 mt-1 mr-2 text-blue-500 fill-current" viewBox="0 0 20 20">
+                          <path d="M15 15H2V6h2.595s.689-.896 2.17-2H1c-.553 0-1 .449-1 1v11c0 .553.447 1 1 1h15c.553 0 1-.447 1-1v-3.746l-2 1.645V15zm-1.639-6.95v3.551L20 6.4l-6.639-4.999v3.131C5.3 4.532 5.3 12.5 5.3 12.5c2.282-3.748 3.686-4.45 8.061-4.45z" />
+                        </svg>
+                      </div>
+                      <div>{r?.href?.slice(0, 30)}...</div>
+                    </div>
                   </div>
                 </div>
               );
