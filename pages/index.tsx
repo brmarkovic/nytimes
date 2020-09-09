045b4775257ddd782640bc210ad25609ba8d777c
Biljana Markovic <biljka78@gmail.com>
2020-09-09 23:20:48 +0200

    Change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Button, Slider, Select } from 'antd';
 
 export default function Index() {
   return (
-    <div className="p-10 text-center text-6xl text-blue-500">
-      <div>Welcome to Next.js!</div>
-      <Button type="primary">Click me!</Button>
-      <Slider defaultValue={30} />
-      <Select defaultValue="lucy" style={{ width: 120 }} onChange={() => {}}>
-        <Select.Option value="jack">Jack</Select.Option>
-        <Select.Option value="lucy">Lucy</Select.Option>
-        <Select.Option value="disabled" disabled>
-          Disabled
-        </Select.Option>
-        <Select.Option value="Yiminghe">yiminghe</Select.Option>
-      </Select>
+    <div className="flex flex-col">
+      <div className="flex justify-between">
+        <div className="flex">=</div>
+        <div className="flex">the NYtimes</div>
+        <div className="flex">P</div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex">Wednesday</div>
+        <div className="flex">subscribe</div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex">title..</div>
+        <div className="flex">...</div>
+      </div>
     </div>
   );
 }
