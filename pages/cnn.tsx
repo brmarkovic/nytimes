99d6503e71feb149b4b87787d6fbdab83f20a6b2
Biljana Markovic <biljka78@gmail.com>
2020-09-15 19:51:28 +0200

    cnn

--- pages/cnn.tsx
+++ pages/cnn.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import CNNHeader from '@/parts/CNNHeader';
-import Link from 'next/dist/client/link';
+import Link from 'next/link';
 import React from 'react';
 
 export default function CNN() {
@@ -7,9 +8,14 @@ export default function CNN() {
     <div className="flex flex-col">
       <CNNHeader />
       <div>
-        <div className="flex flex-col">
+        <div className="flex flex-col px-2">
           <Link href="/cnn/article1">
             <a>Trump Clanak!</a>
+          </Link>
+        </div>
+        <div className="flex flex-col px-2">
+          <Link href="/cnn/article2">
+            <a>Trump Clanak novosti!</a>
           </Link>
         </div>
       </div>
