11dab3700ad49542b37d442eca8fa3ee56e2d678
Biljana Markovic <biljka78@gmail.com>
2020-09-19 23:16:41 +0200

    b92

--- pages/b92.tsx
+++ pages/b92.tsx
@@ -25,7 +25,7 @@ export default function B92() {
           <div className="px-2 py-4 text-2xl text-gray-700 ">Zidan sokirao Jovica </div>
           <div className="flex justify-start text-gray-500 ">
             <div className="p-2">SPORT</div>
-            <div className="p-2 border-l border-gray-500">KOMENTARA:93 </div>
+            <div className="p-2 border-l border-gray-500">KOMENTARA: 93 </div>
           </div>
         </div>
         <div className="p-5">
@@ -35,7 +35,23 @@ export default function B92() {
           />{' '}
         </div>
       </div>
-      <div>.....</div>
+      <div className="flex py-4 m-4 border-t border-gray-400">
+        <div className="flex flex-col w-2/3">
+          <div className="px-2 py-4 text-xl text-gray-700 ">
+            Izgradnja nove pasarele kod Ade Ciganilje je u toku: Pogledajete kako teku radovi FOTO
+          </div>
+          <div className="flex justify-start text-gray-500 ">
+            <div className="p-2">BIZ</div>
+            <div className="p-2 border-l border-gray-500">KOMENTARA: 13 </div>
+          </div>
+        </div>
+        <div className="p-5">
+          <img
+            src="https://www.b92.net/news/pics/2020/09/19/14553436155f660f74f336f686101722_208x160x202000.jpg"
+            alt=""
+          />{' '}
+        </div>
+      </div>
       <div>.....</div>
       <div>.....</div>
     </div>
