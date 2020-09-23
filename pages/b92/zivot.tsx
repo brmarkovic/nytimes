20114b6b976bae935b1212ff6d33f23660411f89
Biljana Markovic <biljka78@gmail.com>
2020-09-23 19:26:14 +0200

    zivot92

--- pages/b92/zivot.tsx
+++ pages/b92/zivot.tsx
@@ -46,7 +46,25 @@ export default function Zivot() {
             </div>
           </div>
         </div>
-        <div>text</div>
+        <div>
+          <div className="flex border-t border-gray-400">
+            <div className="flex flex-col w-2/3">
+              <div className="px-2 py-4 text-xl text-gray-700 ">
+                Buran zivot srpske rokerke: Uzimala je heroin i udala se posle 10 dana veze
+              </div>
+              <div className="flex justify-start text-gray-500 ">
+                <div className="p-2">VESTI</div>
+                <div className="p-2 border-l border-gray-500">KOMENTARA: 43 </div>
+              </div>
+            </div>
+            <div className="p-5">
+              <img
+                src="https://www.b92.net/news/pics/2020/09/23/7140611705f6b06a30b78b039739990_214x153x353400.jpg"
+                alt=""
+              />{' '}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
