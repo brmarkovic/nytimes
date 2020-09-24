5c0042f8f8e505f5a1f30fbc9f0396e0d7588ca9
Biljana Markovic <biljka78@gmail.com>
2020-09-24 18:20:11 +0200

    dexyco

---
+++ pages/dexyco.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+export default function dexyco() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex text-white bg-red-600">
+        <div className="flex px-2 text-4xl font-bold font bold 1/12">=</div>
+        <div className="flex justify-center w-9/12">
+          <img src="https://www.dexy.co.rs/files/images/logo/dexyco-logo.webp" alt="" />
+        </div>
+        <div className="flex w-1/12 text-3xl">P</div>
+        <div className="flex justify-center w-1/12 text-3xl bg-green-600">K</div>
+      </div>
+      <div className="flex justify-center p-1 font-semibold text-white bg-red-600">PORUCITE TELEFONOM NA</div>
+      <div>
+        <img
+          src="https://www.dexy.co.rs/files/images/slideri/main_sliders800x900/LEGO-rancevi-800-x-900_22.webp"
+          alt=""
+        />
+      </div>
+      <div className="p-4">
+        <img
+          src="https://www.dexy.co.rs/files/images/slideri/novi_slider800x400_680x300/Tocak-srece-800x400.webp"
+          alt=""
+        />
+      </div>
+      <div className="px-4">
+        <img
+          src="https://www.dexy.co.rs/files/images/slideri/novi_slider800x400_680x300/a-b-c_800-x-400(1).webp"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-center p-2 text-xl">Poruci telefonom</div>
+        <div className="p-4">
+          <img
+            src="https://www.dexy.co.rs/files/images/slideri/home_baner800x400/Poruci-telefonom_800-x-400-novi_broj.webp"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex flex-col p-2">
+        <div className="flex justify-center text-xl">Izdvajamo</div>
+        <div className="flex">
+          <div className="w-1/2">
+            {' '}
+            <img
+              src="https://www.dexy.co.rs/files/thumbs/files/images/slike_proizvoda/thumbs_600/CBF240318_600_600px.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/2">
+            <img
+              src="https://www.dexy.co.rs/files/thumbs/files/images/slike_proizvoda/thumbs_600/AB12000011000_600_600px.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex justify-center w-1/2 text-xl">Igracke</div>
+          <div className="flex justify-center w-1/2 text-xl">Bebe</div>
+        </div>
+        <div className="flex">
+          <div className="w-1/2">
+            {' '}
+            <img
+              src="https://www.dexy.co.rs/files/thumbs/files/images/slike_proizvoda/thumbs_600/IM91634_600_600px.jpg"
+              alt=""
+            />
+          </div>
+          <div className="w-1/2">
+            <img
+              src="https://www.dexy.co.rs/files/thumbs/files/images/slike_proizvoda/thumbs_600/AV10071362049_600_600px.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div>123</div>
+      <div>123</div>
+      <div>123</div>
+      <div>123</div>
+      <div>123</div>
+    </div>
+  );
+}
