f496f74f56eacc1e3109839f968a59857514d772
Biljana Markovic <biljka78@gmail.com>
2020-11-15 21:15:42 +0100

    bika

---
+++ pages/bika.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-alert */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+
+const showInfo = true;
+const name = 'Biljana';
+const age = 32;
+const person = {
+  name: 'Biljana',
+  lastname: 'Markovic',
+  country: { name: 'Srbija' },
+};
+
+const valute = [
+  { label: 'Eur', ratio: 1 },
+  { label: 'Rsd', ratio: 185 },
+];
+
+const popUpAlerter = () => {
+  alert('PopUP!');
+};
+
+const PrvaKomponenta = ({ nameInput, ageInput, personInput, valuteInput, showInfoInput, popUpAlerterInput }) => {
+  return (
+   <>
+   <div>
+  Moje ime je {nameInput} ({ageInput})
+   </div>
+   <div>
+    {personInput.name} {personInput.lastname} iz {personInput.country.name}
+   </div>
+   <div>
+    {valuteInput.map((r)=>{
+     return(
+     <>
+      {r.label} ({r.ratio})<br/>
+     </>
+     )
+    })}
+   </div>
+   <button className="mx-10 bg-gray-500 rounded" onClick={popUpAlerterInput}>
+    Alert me!
+   </button>
+  <div>
+  {!showInfoInput&&(
+    <>Osnovni info</>
+   )}
+   {showInfoInput&&(
+    <>Dodatni info</>
+   )}
+  </div>
+   </>
+
+  );
+};
+
+export default function Bika() {
+  return (
+    <>
+      <PrvaKomponenta 
+      nameInput={name}
+      ageInput={age}
+      personInput={person}
+      showInfoInput={showInfo}
+      valuteInput={valute}
+      popUpAlerterInput={popUpAlerter}
+      />
+      <div>Test</div>
+    </>
+  );
+}
