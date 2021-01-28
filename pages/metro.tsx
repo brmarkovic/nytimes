0af1a50be86393e28f6bed48b1dd44595f82ad87
Biljana Markovic <biljka78@gmail.com>
2021-01-28 12:57:35 +0100

    metro start

---
+++ pages/metro.tsx
@@ -0,0 +1,13 @@
+import B92Menu from 'parts/MetroMenu';
+import React from 'react';
+
+export default function Metro() {
+  return (
+    <div>
+      <div className="flex flex-col">
+        <div className="flex ">prva linija</div>
+        metro kreiranje sajta
+      </div>
+    </div>
+  );
+}
