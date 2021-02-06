6c985c0ae5595e1ea5743dbbbf1405a6683434bc
Biljana Markovic <biljka78@gmail.com>
2021-02-06 14:45:49 +0100

    definisanje context,evetn i state

--- parts/XstateSimple12/_machine.ts
+++ parts/XstateSimple12/_machine.ts
@@ -45,9 +45,42 @@ export const backendServer = new ApolloClient({
 // SNIMANJE NA SERVER KRAJ
 // SNIMANJE NA SERVER KRAJ
 
+type Iklijentfirma = {
+  id: number;
+  imefirma: string;
+  pibfirma: number;
+};
+type Iklijentfaktura = {
+  id: number;
+  fakturabroja: number;
+  id_klijentfirma: number;
+};
+type Istavkefakture = {
+  id: number;
+  iznosfaktura: number;
+  pdvfaktura: number;
+  id_faktura: number;
+};
+type Iklijentplacanje = {
+  id: number;
+  datumplacanja: number;
+  iznosplacanja: number;
+  id_klijentfirma: number;
+};
 // Icontext
 export interface Icontext {
   show: boolean;
+  listaklijentfirma: Iklijentfirma[];
+  noviklijentfirma: string;
+  listaklijentfaktura: Iklijentfaktura[];
+  novaklijentfaktura: string;
+  listaklijentplacanje: Iklijentplacanje[];
+  novoklijentplacanje: string;
+  novastavkafakture: string;
+  listastavkefakture: Istavkefakture[];
+  trenutniklijentfirma: number;
+  trenutniklijentfaktura: number;
+  trenutniklijentplacanje: number;
 }
 
 // Ievents
@@ -61,14 +94,123 @@ type evINPUT = {
   data: string;
 };
 
-export type Ievents = evINPUT | evSHOW | { type: 'idle' };
+type evNOVIKLIJENTFIRMA = {
+  type: 'NOVIKLIJENTFIRMA';
+  data: {
+    imefirma: string;
+    pibfirma: number;
+  };
+};
+type evNOVAKLIJENTFAKTURA = {
+  type: 'NOVAKLIJENTFAKTURA';
+  data: {
+    fakturabroj: number;
+    id_klijentfirma: number;
+  };
+};
+type evNOVOKLIJENTPLACANJE = {
+  type: 'NOVOKLIJENTPLACANJE';
+  data: {
+    datum: number;
+    iznos: number;
+    id_klijentfirma: number;
+  };
+};
+type evNOVASTAVKAFAKTURE = {
+  type: 'NOVASTAVKAFAKTURE';
+  data: {
+    iznosfaktura: number;
+    pdvfaktura: number;
+    id_faktura: number;
+  };
+};
+type evKLIJENTFAKTURA = {
+  type: 'KLIJENTFAKTURA';
+  data: {
+    id: number;
+  };
+};
+type evSTAVKEFAKTURE = {
+  type: 'STAVKEFAKTURE';
+  data: {
+    id: number;
+  };
+};
+type evKLIJENTPLACANJE = {
+  type: 'KLIJENTPLACANJE';
+  data: {
+    id: number;
+  };
+};
+type evDODAJNOVIKLIJENTFIRMA = {
+  type: 'DODAJNOVIKLIJENTFIRMA';
+  data: {
+    imefirma: string;
+    pibfirma: number;
+  };
+};
+
+type evDODAJNOVAKLIJENTFAKTURA = {
+  type: 'DODAJNOVAKLIJENTFAKTURA';
+  data: {
+    fakturabroj: number;
+    id_klijentfirma: number;
+  };
+};
+type evDODAJNOVOKLIJENTPLACANJE = {
+  type: 'DODAJNOVOKLIJENTPLACANJE';
+  data: {
+    datum: number;
+    iznos: number;
+    id_klijentfirma: number;
+  };
+};
+type evDODAJNOVASTAVKAFAKTURE = {
+  type: 'DODAJNOVASTAVKAFAKTURE';
+  data: {
+    iznosfaktura: number;
+    pdvfaktura: number;
+    id_faktura: number;
+  };
+};
+export type Ievents =
+  | evINPUT
+  | evSHOW
+  | evNOVIKLIJENTFIRMA
+  | evDODAJNOVIKLIJENTFIRMA
+  | evNOVAKLIJENTFAKTURA
+  | evDODAJNOVAKLIJENTFAKTURA
+  | evKLIJENTFAKTURA
+  | evKLIJENTPLACANJE
+  | evNOVOKLIJENTPLACANJE
+  | evDODAJNOVOKLIJENTPLACANJE
+  | evSTAVKEFAKTURE
+  | evNOVASTAVKAFAKTURE
+  | evDODAJNOVASTAVKAFAKTURE
+  | { type: 'BROWSER' };
 
 const send = (sendEvent: Ievents, sendOptions?: any) => untypedSend(sendEvent, sendOptions);
 
 interface Istates {
   states: {
     ssr: {};
-    idle: {};
+    pregledfirma: {};
+    // firma
+    ucitajklijentfirma: {};
+    vidilistuklijentfirma: {};
+    dodajnovaklijentfirma: {};
+    // faktura
+    ucitajklijentfaktura: {};
+    vidilistaklijentfaktura: {};
+    dodajnovaklijentfaktura: {};
+    // stavke fakture
+    ucitajstavkefaktura: {};
+    vidilistustavkefakture: {};
+    dodajnovastavkafakture: {};
+    // placanje
+    ucitaklijentplacanje: {};
+    vidilistuklijentplacanje: {};
+    dodajnovoklijentplacanje: {};
   };
 }
 
@@ -78,29 +220,44 @@ export const XstateSimple12Machine = Machine<Icontext, Istates, Ievents>({
   // BIKA FOKUS >>>>>>>>>>
   context: {
     show: false,
+    noviklijentfirma: '',
+    listaklijentfirma: [],
+    novaklijentfaktura: '',
+    listaklijentfaktura: [],
+    novoklijentplacanje: '',
+    listaklijentplacanje: [],
+    novastavkafakture: '',
+    listastavkefakture: [],
+    trenutniklijentfirma: 1,
+    trenutniklijentfaktura: 1,
+    trenutniklijentplacanje: 1,
   },
   // BIKA FOKUS END <<<<<<
   states: {
     // DEFAULT MILAN STATE
     ssr: {
       on: {
-        idle: [
+        BROWSER: [
           {
-            target: 'idle',
+            target: 'pregledfirma',
           },
         ],
       },
     },
-    idle: {
-      on: {
-        SHOW: {
-          actions: [
-            assign((cx, ev: evSHOW) => {
-              cx.show = ev.data;
-            }),
-          ],
-        },
-      },
+    pregledfirma: {
+      on: {},
     },
+    ucitajklijentfirma: {},
+    vidilistuklijentfirma: {},
+    dodajnovaklijentfirma: {},
+    ucitajklijentfaktura: {},
+    vidilistaklijentfaktura: {},
+    dodajnovaklijentfaktura: {},
+    ucitajstavkefaktura: {},
+    vidilistustavkefakture: {},
+    dodajnovastavkafakture: {},
+    ucitaklijentplacanje: {},
+    vidilistuklijentplacanje: {},
+    dodajnovoklijentplacanje: {},
   },
 });
